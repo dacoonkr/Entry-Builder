26c4829044ebff33c4944f7180de8584ac2d8988
dacoonkr <dacoon0621@gmail.com>
2020-11-19 21:26:06 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entry_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
